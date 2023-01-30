ac9feb7e0b53a536f38aca9c613f8bab395eb2ed
priirocha <prii.rocha@hotmail.com>
2023-01-30 17:29:31 +0000

    Change ConstraintLayout to LinearLayout Added 2 textView Added an EditText Update new textViews programmatically

--- app/src/main/java/com/example/mobileapps1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mobileapps1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myTextView = findViewById<TextView>(R.id.myTextView)
-        val newTextString = getString(R.string.some_text)
-        val textView2 = findViewById<TextView>(R.id.textView2)
-        myTextView.text = getString(R.string.some_text) + "123"
-        val textView3 = findViewById<TextView>(R.id.textView3)
-        val newTextString3 = getString(R.string.some_text) + "987698"
+        val welcomeText = findViewById<TextView>(R.id.welcomeText)
+        welcomeText.text = getString(R.string.hello_dorset)
+
+        val welcomeNameText = findViewById<TextView>(R.id.welcomeNameText)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val nameText = nameEditText.text
+        saveButton.setOnClickListener {
+            val welcomeNameTextString = "Welcome, $nameText!"
+            welcomeNameText.text = welcomeNameTextString
+        }
+
+        val nextPageButton = findViewById<Button>(R.id.nextPageButton)
+        nextPageButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
 
 
     }
